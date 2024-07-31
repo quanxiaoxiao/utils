e57e7606fe5c82e5845719e29fc04aef1aa7b815
quan <quan_xiaoxiao@qq.com>
2024-07-31 17:05:09 +0800

    xxx

--- src/getDataValue.mjs
+++ src/getDataValue.mjs
@@ -22,27 +22,32 @@ export default (str) => {
   const len = str.length;
   const tokenList = [];
   if (startOf !== 0) {
-    tokenList.push({
+    const d = {
       pathList: parse(str, 0, startOf),
       s: str.slice(0, startOf),
       isSub: false,
-    });
+    };
+    d.decode = () => d.pathList;
+    tokenList.push(d);
   }
   {
     const s = str.slice(startOf + 1, endOf);
     if (s.trim() === '') {
-      tokenList.push({
-        pathList: [],
-        s,
-        isSub: true,
-      });
-    } else {
-      tokenList.push({
-        pathList: parse(str, startOf + 1, endOf),
-        s,
-        isSub: true,
-      });
+      return () => null;
     }
+    const d = {
+      pathList: parse(str, startOf + 1, endOf),
+      s,
+      isSub: true,
+    };
+    d.decode = (pathList, data) => {
+      const dataKey = getValueOfPathList(d.pathList)(data);
+      if (typeof dataKey !== 'string' || dataKey === '') {
+        return null;
+      }
+      return [...pathList, dataKey];
+    };
+    tokenList.push(d);
   }
   while (endOf < len - 1) {
     const nextStart = findIndex(str, '[', endOf + 1);
@@ -54,11 +59,13 @@ export default (str) => {
       break;
     }
     if (nextStart !== endOf + 1) {
-      tokenList.push({
+      const d = {
         pathList: parse(str, endOf + 1, nextStart),
         s: str.slice(endOf + 1, nextStart),
         isSub: false,
-      });
+      };
+      d.decode = (pathList) => [...pathList, ...d.pathList];
+      tokenList.push(d);
     }
     startOf = nextStart;
     endOf = nextEnd;
@@ -67,35 +74,41 @@ export default (str) => {
       tokenList.push({
         pathList: [],
         s,
+        decode: () => null,
         isSub: true,
       });
     } else {
+      const pathList = parse(str, startOf + 1, endOf);
       tokenList.push({
-        pathList: parse(str, startOf + 1, endOf),
+        pathList,
         s,
+        decode: (pre, data) => {
+          const dataKey = getValueOfPathList(pathList)(data);
+          if (typeof dataKey !== 'string' || dataKey === '') {
+            return null;
+          }
+          return [...pre, dataKey];
+        },
         isSub: true,
       });
     }
   }
   if (endOf !== len - 1) {
+    const pathList = parse(str, endOf + 1);
     tokenList.push({
-      pathList: parse(str, endOf + 1),
+      pathList,
       s: str.slice(endOf + 1),
+      decode: (pre) => [...pre, ...pathList],
       isSub: false,
     });
   }
   return (data) => {
-    const pathList = [];
+    let pathList = [];
     for (let i = 0; i < tokenList.length; i++) {
       const tokenItem = tokenList[i];
-      if (tokenItem.isSub) {
-        const dataKey = getValueOfPathList(tokenItem.pathList)(data);
-        if (typeof dataKey !== 'string' || dataKey === '') {
-          return null;
-        }
-        pathList.push(dataKey);
-      } else {
-        pathList.push(...tokenItem.pathList);
+      pathList = tokenItem.decode(pathList, data);
+      if (pathList == null) {
+        return null;
       }
     }
     return getValueOfPathList(pathList)(data);
