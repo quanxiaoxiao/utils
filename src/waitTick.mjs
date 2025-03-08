05b9231e315217f6d15d6d5ee37a539b6667c30d
quan <quan_xiaoxiao@qq.com>
2025-03-08 16:08:38 +0800

    xxx

--- src/waitTick.mjs
+++ src/waitTick.mjs
@@ -1,23 +1,11 @@
 import parseInteger from './number/parseInteger.mjs';
 
 export default (time, cb) => {
-  let isEmit = false;
-  let n;
-  if (time != null) {
-    n = parseInteger(time);
-    if (n == null || n < 0) {
-      n = 0;
-    }
-  }
+  const n = Math.max(0, parseInteger(time) || 0);
   const tick = setTimeout(() => {
-    if (cb) {
-      isEmit = true;
-      cb();
-    }
+    cb?.();
   }, n);
   return () => {
-    if (!isEmit) {
-      clearTimeout(tick);
-    }
+    clearTimeout(tick);
   };
 };
